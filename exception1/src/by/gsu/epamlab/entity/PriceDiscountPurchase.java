b40ce57b00925c2db6749d260e453681a3e0609c
AntonUsiankou <74109426+AntonUsiankou@users.noreply.github.com>
2021-07-29 23:14:46 +0300

    Add files via upload

--- exception1/src/by/gsu/epamlab/entity/PriceDiscountPurchase.java
+++ exception1/src/by/gsu/epamlab/entity/PriceDiscountPurchase.java
@@ -1,46 +1,40 @@
 package by.gsu.epamlab.entity;
 
-import java.util.Scanner;
+import static by.gsu.epamlab.constants.Constants.*;
 
 public class PriceDiscountPurchase extends Purchase {
 
     private Byn priceDiscount;
-    private static final int NULL_NUMBER = 0;
-
-    private static final String NON_POSITIVE_VALUE_MES = "non positive value ";
-    private static final String IN_DISCOUNT_MESSAGE = "in discount";
 
     public PriceDiscountPurchase() {
     }
 
-    public PriceDiscountPurchase(String name, int price, int number, int priceDiscount) {
-        super(name, price, number);
-        this.priceDiscount = new Byn(priceDiscount);
+    public PriceDiscountPurchase(String productName, int price, int numberUnits, int priceDiscount) {
+        super(productName, price, numberUnits);
+        setPriceDiscount(priceDiscount);
     }
 
+    private void setPriceDiscount(int priceDiscount) {
+        this.priceDiscount = new Byn(priceDiscount);
+    }
 
     public Byn getPriceDiscount() {
         return priceDiscount;
     }
 
-    public void setPriceDiscount(int kopeckDiscount) {
-        if (kopeckDiscount <= NULL_NUMBER) {
-           throw new IllegalArgumentException(NON_POSITIVE_VALUE_MES + priceDiscount + IN_DISCOUNT_MESSAGE);
-        }
-            this.priceDiscount = new Byn(kopeckDiscount);
-    }
     @Override
-    protected String getDiscount(){
+    protected String fieldsToString() {
+        return super.fieldsToString() + DELIMITER + priceDiscount;
+    }
+
+    @Override
+    protected String getDiscount() {
         return getPriceDiscount().toString();
     }
 
     @Override
-    protected String fieldsToString() {
-        return super.fieldsToString() + ";" + priceDiscount;
-    }
-
-    @Override
     public Byn getCost() {
-        return new Byn(getPrice()).subtraction(priceDiscount).multiply(getNumber());
+        Byn cost = new Byn(getPrice());
+        return cost.subtraction(priceDiscount).multiply(getNumber());
     }
 }
--- exception1/src/by/gsu/epamlab/entity/PurchasesList.java
+++ exception1/src/by/gsu/epamlab/entity/PurchasesList.java
@@ -1,47 +1,40 @@
 package by.gsu.epamlab.entity;
 
+import by.gsu.epamlab.exceptions.WrongLineException;
+import by.gsu.epamlab.factories.ComparatorFactory;
+import by.gsu.epamlab.factories.PurchasesFactory;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
 
+import static by.gsu.epamlab.constants.Constants.*;
+
 public class PurchasesList {
-
-    private final static String FILE_PATH = "src/";
-    private final static String FILE_EXTENSION = ".csv";
-
-    private final static String FILE_READER_ERROR_MESSAGE = "The file you are looking for does not exist.";
-    private final static String DELETING_INDEX_ERROR_MESSAGE = "Error get Purchase: invalid index";
-    private final static String TOTAL_COAST_PRINT_MESSAGE = "Total cost %26s\n";
-    private final static String FORMATTING_TABLE_PATTERN = "%-6s %5s %5s %5s %5s\n";
-    private final static String NAME_COLUME = "Name";
-    private final static String PRICE_COLUME = "Price";
-    private final static String NUMBER_COLUME = "Number";
-    private final static String DISCOUNT_COLUME = "Discount";
-    private final static String COST_COLUME = "Cost";
-
-    private static final int NUMBER_NULL = 0;
-    private static final int NUMBER_ONE = 1;
-    private static final int NUMBER_ONE_HUNDRED = 100;
 
     private List<Purchase> purchases;
 
     public PurchasesList() {
-        purchases = new ArrayList<Purchase>();
+        purchases = new ArrayList<>();
     }
 
     public PurchasesList(String fileName) {
-        this();
 
-        try (Scanner scanner = new Scanner(new FileReader(FILE_PATH + fileName + FILE_EXTENSION))) {
+
+        try (Scanner scanner = new Scanner(new FileReader(PATH + fileName + EXTENSION))) {
+            purchases=new ArrayList<>();
             scanner.useLocale(Locale.ENGLISH);
-            while(scanner.hasNext()){
-                Purchase purchase = PurchasesFactory.getPurchaseFromFactory(scanner);
-                if(purchase != null){
-                    purchases.add(purchase);
+            while (scanner.hasNextLine()) {
+            String line=scanner.nextLine();
+                try {
+                    purchases.add(PurchasesFactory.getPurchaseFromFactory(line));
+                } catch (WrongLineException e) {
+                    System.err.println(line+  HYPHEN + e.getMessage());
                 }
             }
         } catch (FileNotFoundException e) {
             System.err.println(FILE_READER_ERROR_MESSAGE);
+            purchases=new ArrayList<>();
         }
     }
 
@@ -49,75 +42,62 @@ public class PurchasesList {
         return purchases.size();
     }
 
-    public boolean isInvalidIndex(int index){
-        return index < NUMBER_NULL || index >= purchases.size();
-    }
-
-    public void insert (int index, Purchase purchase){
-        if(isInvalidIndex(index)){
-            index = purchases.size() - NUMBER_ONE;
-        }
-        purchases.add(index, purchase);
-    }
-
-    public void delete(int index){
-        if(isInvalidIndex(index)){
-            System.out.println(DELETING_INDEX_ERROR_MESSAGE);
-            //return;
-        }
-        purchases.remove(index);
-    }
-    public Purchase getPurchaseByIndex(int index) {
-
-        if (isInvalidIndex(index)) {
-            System.err.println("Error get Purchase: invalid index");
-            return null;
-        }
-        return purchases.get(index);
-    }
-
     public void printList() {
+
         System.out.printf(FORMATTING_TABLE_PATTERN, NAME_COLUME, PRICE_COLUME, NUMBER_COLUME, DISCOUNT_COLUME, COST_COLUME);
-        for(Purchase purchase : purchases){
+        for (Purchase purchase : purchases) {
             System.out.println(purchase.print());
         }
         System.out.printf(TOTAL_COAST_PRINT_MESSAGE, getTotalCost());
     }
 
-    public Byn getTotalCost (){
-        Byn totalCost = new Byn();
+    public Byn getTotalCost() {
 
-        for (Purchase purchase : purchases){
+        Byn totalCost = new Byn(NUMBER_NULL);
+
+        for (Purchase purchase : purchases) {
             totalCost.addition(purchase.getCost());
         }
+
         return totalCost;
     }
 
-    public void sort(Comparator<Purchase> comparator){
-        purchases.sort(comparator);
-    }
-
-    public int search (String productName, Byn price, int numberUnits){
-        return search(productName,price, numberUnits, null);
-    }
-
-    public int search (Purchase purchase){
-        return Collections.binarySearch(purchases, purchase, Collections.reverseOrder());
-    }
-
-    public int search(String productName, Byn price, int numberUnits, Byn discount){
-
-        int priceCoins = price.getRubs() * NUMBER_ONE_HUNDRED + price.getCoins();
-
-        Purchase purchase;
-
-        if(discount != null){
-            int discountCoins = discount.getRubs() * NUMBER_ONE_HUNDRED + discount.getCoins();
-            purchase = new PriceDiscountPurchase(productName, priceCoins,numberUnits, discountCoins);
-        }else{
-            purchase = new Purchase(productName, priceCoins, numberUnits);
+    public void insert(int index, Purchase purchase) {
+        if (isInvalidIndex(index)) {
+            index = purchases.size() - NUMBER_ONE;
         }
+        purchases.add(index, purchase);
+    }
 
-        return search(purchase);
+    private boolean isInvalidIndex(int index) {
+        return index < NUMBER_NULL || index >= purchases.size();
+    }
+
+    public void delete(int index) {
+        if (isInvalidIndex(index)) {
+            return;
+        }
+        purchases.remove(index);
+    }
+
+    public Purchase getPurchaseByIndex(int index) {
+        if (isInvalidIndex(index)) {
+            return null;
+        }
+        return purchases.get(index);
+    }
+
+    public void sort() {
+        Collections.sort(purchases, ComparatorFactory.getPurchaseComparator());
+    }
+
+    public void search(Purchase purchase) {
+
+        int index = Collections.binarySearch(purchases, purchase, ComparatorFactory.getPurchaseComparator());
+        if (index < 0) {
+            System.out.println(IS_NOT_FOUND_MESSAGE);
+        } else {
+            System.out.println(IS_FOUND_IN_POS_MESSAGE + index);
+        }
     }
 }
